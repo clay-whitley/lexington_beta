74c26fd2685a4d5444245c45ef0e28c8c2950810
Clay Whitley <clay.aaron.whitley@gmail.com>
2014-06-13 18:26:00 -0700

    setup mongo connection env variable

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ var http = require('http');
 var path = require('path');
 
 var mongoose = require('mongoose');
-mongoose.connect(process.env.DOMAIN || 'mongodb://localhost/test');
+mongoose.connect(process.env.MONGO
+  DOMAIN || 'mongodb://localhost/test');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
